0f5120cd161f8bde61659e22bd557922c9a7cf53
osalysdecatarina <72741076+osalysdecatarina@users.noreply.github.com>
2020-12-03 16:50:33 +0100

    Ajoutez des fichiers projet.

---
+++ palin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace palin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool palindrome = true;
+            Console.WriteLine("Veuillez entrer le mot ou la phrase");
+            string reponse = Console.ReadLine();
+            reponse = reponse.Split('.')[0];
+            for (int i = 0; i < reponse.Length; i++)
+            {
+                if (reponse[i] != reponse[(reponse.Length-1) - i])
+                {
+                    palindrome = false ;
+                }
+
+            }
+            if (palindrome ==true)
+            {
+                Console.WriteLine("Ce mot ou cette phrase est un palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Ce mot ou cette phrase n'est pas un palindrome");
+            }
+        }
+    }
+}
